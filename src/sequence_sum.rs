35931f4a7f1ee1aad3d62c7654193d58b92fa59d
cprohoda <cgprohoda@gmail.com>
2023-11-25 00:13:31 -0500

    Taylor sum

--- src/sequence_sum.rs
+++ src/sequence_sum.rs
@@ -10,6 +10,24 @@ pub fn main() {
             },
         ),
     );
+
+    let taylor_num = 0.8;
+    println!("{:.32}", taylor_num);
+    println!(
+        "x: {:?} -> {:.32}",
+        taylor_num,
+        taylor_sum(taylor_num),
+    );
+}
+
+pub fn taylor_sum(x: f64) -> f64 {
+    if 0. < x && x < 1. {
+        1./(1.-x)
+    } else {
+        partial_sum(1_000, |n| {
+            x.powi(n as i32)
+        })
+    }
 }
 
 pub fn partial_sum<F: Fn(usize) -> f64>(n: usize, sequence: F) -> f64 {
