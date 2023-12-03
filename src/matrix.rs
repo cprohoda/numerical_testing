a084db5149d2e0134d562ee127217a33ce24a066
cprohoda <cgprohoda@gmail.com>
2023-12-03 01:49:11 -0500

    Matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,11 +1,14 @@
 pub fn main() {
-    println!("{:?}", Matrix::<2, 2>::new(0.));
-    println!("{:?}", Matrix::<2, 2>::new_matrix([[1., 1.], [2., 3.]]));
+    let a = Matrix::<2, 2>::new_matrix([[1., 0.], [0., 1.]]);
+    let b = Matrix::<2, 2>::new_matrix([[1., 1.], [2., 3.]]);
+    println!("A: {:?}", a);
+    println!("B: {:?}", b);
+    println!("Result: {:?}", a.multiply(b));
 }
 
 #[derive(Debug)]
 pub struct Matrix<const M: usize, const N: usize> {
-    data: [[f64; M]; N],
+    pub data: [[f64; M]; N],
 }
 
 impl<const M: usize, const N: usize> Matrix<M, N> {
@@ -19,5 +22,19 @@ impl<const M: usize, const N: usize> Matrix<M, N> {
         Self {
             data: data,
         }
+    }
+
+    pub fn multiply(&self, other: Matrix<N, M>) -> Matrix<M, M> {
+        let mut result = Matrix::<M, M>::new(0.);
+
+        result.data.iter_mut().enumerate().for_each(|(i, mut row)| {
+            row.iter_mut().enumerate().for_each(|(j, mut value)| {
+                *value = self.data[i].iter().enumerate().map(|(k, val)| {
+                    val * other.data[j][k]
+                }).sum();
+            });
+        });
+
+        result
     }
 }
