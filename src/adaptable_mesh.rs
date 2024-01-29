50878065c5f4ad2c22effdcf59ee200adc877d62
cprohoda <cgprohoda@gmail.com>
2024-01-28 19:50:34 -0500

    Fix adaptable mesh init; doesn't need MaybeUninit

--- src/adaptable_mesh.rs
+++ src/adaptable_mesh.rs
@@ -1,30 +1,28 @@
-use std::mem::{transmute, MaybeUninit};
-
 struct AdaptableMesh<const X: usize, const Y: usize, const Z: usize, T: Default + Copy, F: Fn(T) -> T> {
     mesh: [[[MeshNode<T>; X]; Y]; Z],
+    dimensions: Vec3,
     f: F,
 }
 
 impl<const X: usize, const Y: usize, const Z: usize, T: Default + Copy, F: Fn(T) -> T> AdaptableMesh<X, Y, Z, T, F> {
-    fn new(update_function: F, lengths: Vec3) -> Self {
-        let mut mesh: [[[MaybeUninit<MeshNode<T>>; X]; Y]; Z] = unsafe { MaybeUninit::uninit().assume_init() };
+    fn new(update_function: F, dimensions: Vec3) -> Self {
+        let mut mesh = [[[MeshNode::<T>::default(); X]; Y]; Z];
 
         (0..X).for_each(|x| {
             (0..Y).for_each(|y| {
                 (0..Z).for_each(|z| {
-                    (&mut mesh[x][y][z]).write(MeshNode::new(Vec3 {
-                        x: (x as f64) * lengths.x / (X as f64),
-                        y: (y as f64) * lengths.y / (Y as f64),
-                        z: (z as f64) * lengths.z / (Z as f64),
-                    }));
+                    mesh[x][y][z].point = Vec3 {
+                        x: (x as f64) * dimensions.x / (X as f64),
+                        y: (y as f64) * dimensions.y / (Y as f64),
+                        z: (z as f64) * dimensions.z / (Z as f64),
+                    };
                 })
             })
         });
 
-        unsafe { transmute::<_, [[[MeshNode<T>; X]; Y]; Z]>(mesh) };
-
         Self {
             mesh: mesh,
+            dimensions: dimensions,
             f: update_function,
         }
     }
