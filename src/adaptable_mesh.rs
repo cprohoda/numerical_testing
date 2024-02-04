647e2a49f1dd69b28cc867dd549742149574a5bb
cprohoda <cgprohoda@gmail.com>
2024-02-04 17:58:38 -0500

    Update step size calc to get whole range

--- src/adaptable_mesh.rs
+++ src/adaptable_mesh.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use std::cmp::max;
 
 struct AdaptableMesh<const X: usize, const Y: usize, const Z: usize, T: Default + Copy + fmt::Debug, F: Fn(T) -> T> {
     mesh: Box<[[[MeshNode<T>; X]; Y]; Z]>,
@@ -10,13 +11,17 @@ impl<const X: usize, const Y: usize, const Z: usize, T: Default + Copy + fmt::De
     fn new(update_function: F, dimensions: Vec3) -> Self {
         let mut mesh = Box::new([[[MeshNode::<T>::default(); X]; Y]; Z]);
 
+        let x_step = dimensions.x / max(X as i64 - 1, 1) as f64;
+        let y_step = dimensions.y / max(Y as i64 - 1, 1) as f64;
+        let z_step = dimensions.z / max(Z as i64 - 1, 1) as f64;
+
         (*mesh).iter_mut().enumerate().for_each(|(z, xy_mesh)| {
             xy_mesh.iter_mut().enumerate().for_each(|(y, x_mesh)| {
                 x_mesh.iter_mut().enumerate().for_each(|(x, mesh_node)| {
                     mesh_node.point = Vec3 {
-                        x: (x as f64) * dimensions.x / (X as f64),
-                        y: (y as f64) * dimensions.y / (Y as f64),
-                        z: (z as f64) * dimensions.z / (Z as f64),
+                        x: (x as f64) * x_step,
+                        y: (y as f64) * y_step,
+                        z: (z as f64) * z_step,
                     };
                 });
             });
@@ -66,7 +71,6 @@ pub fn main() {
         |f| f.powi(2) + 1.0,
         Vec3 {x: 10.0, y: 10.0, z: 10.0},
     );
-    println!("{:?}", simulation);
     simulation.update();
     println!("{:?}", simulation);
 }
