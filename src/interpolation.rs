1445c120db2a23b87ebeb272c5bcb84f421273f1
cprohoda <cgprohoda@gmail.com>
2024-03-06 18:06:16 -0500

    Implement 3d lagrange interpolating polynomial

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -40,6 +40,21 @@ pub fn lagrange_elementary_polynomial_three(x_N: [f64; 3], y_N: [f64; 3]) -> imp
     }
 }
 
+pub fn lagrange_interpolating_polynomial_3d(x_n: [f64; 3], y_n: [f64; 3], z_n: [[f64; 3]; 3]) -> impl Fn(f64, f64) -> f64 {
+    move |x, y| {
+        let li_0 = |x| (x - x_n[2])/(x_n[0] - x_n[2]) * (x - x_n[1])/(x_n[0] - x_n[1]);
+        let li_1 = |x| (x - x_n[0])/(x_n[1] - x_n[0]) * (x - x_n[2])/(x_n[1] - x_n[2]);
+        let li_2 = |x| (x - x_n[0])/(x_n[2] - x_n[0]) * (x - x_n[1])/(x_n[2] - x_n[1]);
+        let lj_0 = |y| (y - y_n[2])/(y_n[0] - y_n[2]) * (y - y_n[1])/(y_n[0] - y_n[1]);
+        let lj_1 = |y| (y - y_n[0])/(y_n[1] - y_n[0]) * (y - y_n[2])/(y_n[1] - y_n[2]);
+        let lj_2 = |y| (y - y_n[0])/(y_n[2] - y_n[0]) * (y - y_n[1])/(y_n[2] - y_n[1]);
+
+        li_0(x) * lj_0(y) * z_n[0][0] + li_1(x) * lj_0(y) * z_n[1][0] + li_2(x) * lj_0(y) * z_n[2][0] +
+        li_0(x) * lj_1(y) * z_n[0][1] + li_1(x) * lj_1(y) * z_n[1][1] + li_2(x) * lj_1(y) * z_n[2][1] +
+        li_0(x) * lj_2(y) * z_n[0][2] + li_1(x) * lj_2(y) * z_n[1][2] + li_2(x) * lj_2(y) * z_n[2][2]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,5 +69,24 @@ mod tests {
         assert_eq!(polynomial(x_N[0]), y_N[0]);
         assert_eq!(polynomial(x_N[1]), y_N[1]);
         assert_eq!(polynomial(x_N[2]), y_N[2]);
+    }
+    
+    #[test]
+    fn test_lagrange_interpolating_polynomial_3d() {
+        let x_n = [0.0, 1.0, 2.0];
+        let y_n = [0.0, 1.0, 2.0];
+        let z_n = [[0.0, 1.0, 3.0], [2.0, 4.0, 8.0], [5.0, 10.0, 16.0]];
+
+        let polynomial = lagrange_interpolating_polynomial_3d(x_n, y_n, z_n);
+
+        assert_eq!(polynomial(x_n[0], y_n[0]), z_n[0][0]);
+        assert_eq!(polynomial(x_n[1], y_n[0]), z_n[1][0]);
+        assert_eq!(polynomial(x_n[2], y_n[0]), z_n[2][0]);
+        assert_eq!(polynomial(x_n[0], y_n[1]), z_n[0][1]);
+        assert_eq!(polynomial(x_n[1], y_n[1]), z_n[1][1]);
+        assert_eq!(polynomial(x_n[2], y_n[1]), z_n[2][1]);
+        assert_eq!(polynomial(x_n[0], y_n[2]), z_n[0][2]);
+        assert_eq!(polynomial(x_n[1], y_n[2]), z_n[1][2]);
+        assert_eq!(polynomial(x_n[2], y_n[2]), z_n[2][2]);
     }
 }
