4e717dbe01b40c27d23996a35752a733e57624ed
cprohoda <cgprohoda@gmail.com>
2024-03-06 18:23:39 -0500

    Create a plot of a 2d interpolation

--- src/plots.rs
+++ src/plots.rs
@@ -1,7 +1,8 @@
 use plotters::prelude::*;
 
 pub fn main() {
-    basic_plot();
+    // basic_plot();
+    interpolation_2d_plot();
 }
 
 pub fn basic_plot() {
@@ -15,6 +16,25 @@ pub fn basic_plot() {
 
     chart.draw_series(LineSeries::new(
         (-314..314).map(|x| x as f64 / 100.0).map(|x| (x, x.sin())),
+        &BLACK,
+    )).unwrap();
+}
+
+pub fn interpolation_2d_plot() {
+    use crate::interpolation::lagrange_elementary_polynomial_three;
+
+    let root_area = BitMapBackend::new("target/interpolation_2d.png", (1000, 1000)).into_drawing_area();
+
+    root_area.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_area)
+        .build_cartesian_2d(0.0..10.0, 0.0..10.0)
+        .unwrap();
+
+    let polynomial = lagrange_elementary_polynomial_three([0.0, 3.0, 5.0], [3.2, 3.1, 5.0]);
+
+    chart.draw_series(LineSeries::new(
+        (0..100).map(|x| x as f64 / 10.0).map(|x| (x, polynomial(x))),
         &BLACK,
     )).unwrap();
 }
