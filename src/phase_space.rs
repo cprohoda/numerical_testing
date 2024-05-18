90e065069609fe232b91ad7509fcbed267b0aa12
cprohoda <cgprohoda@gmail.com>
2024-05-18 14:02:25 -0400

    WIP phase space plotting

--- src/phase_space.rs
+++ src/phase_space.rs
@@ -1,28 +1,20 @@
 use std::f64::consts::PI;
 
+use plotters::{backend::BitMapBackend, chart::{ChartBuilder, LabelAreaPosition}, drawing::IntoDrawingArea, series::LineSeries, style::{BLACK, WHITE}};
+
 pub fn main() {
-    pendulum();
+    phase_space_plot();
 }
 
-fn pendulum() {
-    let steps = 10000;
+fn pendulum(mut theta: f64, mut theta_dot: f64) -> (f64, f64) {
     let delta_t = 1.0/100.0;
 
-    let mut theta = PI/4.0;
-    let mut theta_dot = 0.0;
+    let theta_double_dot = get_theta_double_dot(theta, theta_dot);
 
-    println!("theta: {:?}      theta_dot: {:?}", theta, theta_dot);
+    theta += theta_dot * delta_t;
+    theta_dot += theta_double_dot * delta_t;
 
-    for step in 0..steps {
-        let t = step as f64 * delta_t;
-
-        let theta_double_dot = get_theta_double_dot(theta, theta_dot);
-
-        theta += theta_dot * delta_t;
-        theta_dot += theta_double_dot * delta_t;
-
-        println!("theta: {:?}      theta_dot: {:?}", theta, theta_dot);
-    }
+    (theta, theta_dot)
 }
 
 fn get_theta_double_dot(theta: f64, theta_dot: f64) -> f64 {
@@ -31,4 +23,27 @@ fn get_theta_double_dot(theta: f64, theta_dot: f64) -> f64 {
     let mu = 0.1;
 
     return -mu * theta_dot - (g / l) * f64::sin(theta);
+}
+
+fn phase_space_plot() {
+    let root_area = BitMapBackend::new("target/pendulum_phase.png", (1000, 1000)).into_drawing_area();
+
+    root_area.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_area)
+        .caption("Pendulum Phase Space", ("Arial", 30))
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .build_cartesian_2d(0.0..10.0, 0.0..10.0)
+        .unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    chart.draw_series(LineSeries::new(
+        (0..100).scan(
+            (3.0, 0.0),
+            |(theta, theta_dot), _x| Some(pendulum(*theta, *theta_dot)),
+        ),
+        &BLACK,
+    )).unwrap();
 }
