c522756c69e037d06dbfeabab59b5a4d7f8a573e
cprohoda <cgprohoda@gmail.com>
2024-05-26 21:44:12 -0400

    4 point discrete fourier transform dft4

--- src/fft.rs
+++ src/fft.rs
@@ -1,9 +1,8 @@
 use std::ops::{Add, Mul};
 
-use nalgebra::{DMatrix, Matrix};
-
 // Only for signed numbers, e.g. floats and ints
 // TODO make generic over those types
+#[derive(Debug)]
 struct Complex {
     r: f64,
     i: f64,
@@ -33,11 +32,27 @@ impl Mul for Complex {
 
 pub fn main() {
     println!("dft2([1.0, 3.0]): {:?}", dft2([1.0, 3.0]));
+    println!
+        ("dft4([1.0, 0.0, 3.0, 0.0]): {:?}",
+        dft4([1.0, 0.0, 3.0, 0.0]),
+    );
 }
 
 pub fn dft2(signal: [f64; 2]) -> [f64; 2] {
     let a = signal;
     [a[0] + a[1], a[0] - a[1]]
+}
+
+pub fn dft4(signal: [Complex; 4]) -> [Complex; 4] {
+    let a = signal;
+    let i = Complex{ r: 0.0, i: 1.0 };
+
+    [
+        a[0] + a[1] + a[2] + a[3],
+        a[0] - i * a[1] - a[2] + a[3],
+        a[0] - a[1] + a[2] - a[3],
+        a[0] + i * a[1] - a[2] - i * a[3],
+    ]
 }
 
 pub fn fft() {
